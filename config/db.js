4b27b19c6d79d3d363a64c97d7dbab2c2f3f9d09
abhijeetpandit7 <abhijeetpandit7@gmail.com>
2023-04-25 14:59:53 +0000

    Add replit config

--- config/db.js
+++ config/db.js
@@ -9,6 +9,6 @@ exports.connectDB = async () => {
     console.log("MongoDB connected: " + connection.connection.host);
   } catch (error) {
     console.error("Error " + error.message);
-    process.exit(1);
+    process.kill(1);
   }
 };

39131d92c5766c47750dc0172fe6f80a4a850293
Abhijeet Pandit <abhijeetpandit7@gmail.com>
2025-06-28 12:10:40 +0530

    Refactor countdown, customization, note, quoteCollection, todo, and todoList controllers to remove catchError and streamline async functions

--- controller/note.js
+++ controller/note.js
@@ -30,88 +30,76 @@ const deleteNote = async (req, res, next) => {
   });
 };
 
-const mergeNote = async (next, sourceUserId, destinationUserId) => {
-  catchError(next, async () => {
-    let noteOfSourceUser = await Note.findById(sourceUserId);
-    if (!noteOfSourceUser) {
-      return {
-        success: true,
-        message: "No document to merge",
-      };
-    } else if (noteOfSourceUser.itemList.length === 0) {
-      await Note.findByIdAndDelete(sourceUserId);
-      return {
-        success: true,
-        message: "No itemList to merge",
-      };
-    }
+const mergeNote = async (sourceUserId, destinationUserId) => {
+  let noteOfSourceUser = await Note.findById(sourceUserId);
+  if (!noteOfSourceUser) {
+    return {
+      success: true,
+      message: "No document to merge",
+    };
+  } else if (noteOfSourceUser.itemList.length === 0) {
+    await Note.findByIdAndDelete(sourceUserId);
+    return {
+      success: true,
+      message: "No itemList to merge",
+    };
+  }
 
-    let noteOfDestinationUser = await Note.findById(destinationUserId);
-    if (!noteOfDestinationUser) {
-      newUserNote = new Note({
-        _id: destinationUserId,
-        itemList: [...noteOfSourceUser.itemList],
-      });
-      await newUserNote.save();
-      await Note.findByIdAndDelete(sourceUserId);
-
-      return {
-        success: true,
-      };
-    }
-
-    noteOfDestinationUser = _.extend(noteOfDestinationUser, {
-      itemList: addOrMergeArrayElements(
-        noteOfDestinationUser.itemList,
-        noteOfSourceUser.itemList,
-        "_id"
-      ),
+  let noteOfDestinationUser = await Note.findById(destinationUserId);
+  if (!noteOfDestinationUser) {
+    newUserNote = new Note({
+      _id: destinationUserId,
+      itemList: [...noteOfSourceUser.itemList],
     });
-    await noteOfDestinationUser.save();
+    await newUserNote.save();
     await Note.findByIdAndDelete(sourceUserId);
 
     return {
       success: true,
     };
+  }
+
+  noteOfDestinationUser = _.extend(noteOfDestinationUser, {
+    itemList: addOrMergeArrayElements(
+      noteOfDestinationUser.itemList,
+      noteOfSourceUser.itemList,
+      "_id"
+    ),
   });
+  await noteOfDestinationUser.save();
+  await Note.findByIdAndDelete(sourceUserId);
+
+  return {
+    success: true,
+  };
 };
 
-const updateNote = async (req, res, next, sendResponse = true) => {
-  catchError(next, async () => {
-    const { userId } = req;
-    if (req.body.data?.notes) data = req.body.data.notes;
-    else data = req.body.data.note;
-    const itemListData = _.isArray(data)
-      ? data.map((item) => renameObjectKey(item, "id", "_id"))
-      : [renameObjectKey(data, "id", "_id")];
+const updateNote = async (req) => {
+  const { userId } = req;
+  if (req.body.data?.notes) data = req.body.data.notes;
+  else data = req.body.data.note;
+  const itemListData = _.isArray(data)
+    ? data.map((item) => renameObjectKey(item, "id", "_id"))
+    : [renameObjectKey(data, "id", "_id")];
 
-    let note = await Note.findById(userId);
+  let note = await Note.findById(userId);
 
-    if (!note) {
-      const newUserNote = new Note({
-        _id: userId,
-        itemList: [...itemListData],
-      });
-      await newUserNote.save();
-
-      if (sendResponse) {
-        return res.status(201).json({ success: true });
-      } else {
-        return newUserNote;
-      }
-    }
-
-    note = _.extend(note, {
-      itemList: addOrMergeArrayElements(note.itemList, itemListData, "_id"),
+  if (!note) {
+    const newUserNote = new Note({
+      _id: userId,
+      itemList: [...itemListData],
     });
-    await note.save();
+    await newUserNote.save();
 
-    if (sendResponse) {
-      return res.json({ success: true });
-    } else {
-      return note;
-    }
+    return { success: true };
+  }
+
+  note = _.extend(note, {
+    itemList: addOrMergeArrayElements(note.itemList, itemListData, "_id"),
   });
+  await note.save();
+
+  return { success: true };
 };
 
 module.exports = {
--- controller/todo.js
+++ controller/todo.js
@@ -18,7 +18,7 @@ const deleteTodo = async (req, res, next) => {
     }
 
     if (req.body?.todoSettings) {
-      await updateCustomization(req, res, next, false);
+      await updateCustomization(req);
     }
 
     let todo = await Todo.findById(userId);
@@ -35,92 +35,80 @@ const deleteTodo = async (req, res, next) => {
   });
 };
 
-const mergeTodo = async (next, sourceUserId, destinationUserId) => {
-  catchError(next, async () => {
-    let todoOfSourceUser = await Todo.findById(sourceUserId);
-    if (!todoOfSourceUser) {
-      return {
-        success: true,
-        message: "No document to merge",
-      };
-    } else if (todoOfSourceUser.itemList.length === 0) {
-      await Todo.findByIdAndDelete(sourceUserId);
-      return {
-        success: true,
-        message: "No itemList to merge",
-      };
-    }
+const mergeTodo = async (sourceUserId, destinationUserId) => {
+  let todoOfSourceUser = await Todo.findById(sourceUserId);
+  if (!todoOfSourceUser) {
+    return {
+      success: true,
+      message: "No document to merge",
+    };
+  } else if (todoOfSourceUser.itemList.length === 0) {
+    await Todo.findByIdAndDelete(sourceUserId);
+    return {
+      success: true,
+      message: "No itemList to merge",
+    };
+  }
 
-    let todoOfDestinationUser = await Todo.findById(destinationUserId);
-    if (!todoOfDestinationUser) {
-      newUserTodo = new Todo({
-        _id: destinationUserId,
-        itemList: [...todoOfSourceUser.itemList],
-      });
-      await newUserTodo.save();
-      await Todo.findByIdAndDelete(sourceUserId);
-
-      return {
-        success: true,
-      };
-    }
-
-    todoOfDestinationUser = _.extend(todoOfDestinationUser, {
-      itemList: addOrMergeArrayElements(
-        todoOfDestinationUser.itemList,
-        todoOfSourceUser.itemList,
-        "_id"
-      ),
+  let todoOfDestinationUser = await Todo.findById(destinationUserId);
+  if (!todoOfDestinationUser) {
+    const newUserTodo = new Todo({
+      _id: destinationUserId,
+      itemList: [...todoOfSourceUser.itemList],
     });
-    await todoOfDestinationUser.save();
+    await newUserTodo.save();
     await Todo.findByIdAndDelete(sourceUserId);
 
     return {
       success: true,
     };
+  }
+
+  todoOfDestinationUser = _.extend(todoOfDestinationUser, {
+    itemList: addOrMergeArrayElements(
+      todoOfDestinationUser.itemList,
+      todoOfSourceUser.itemList,
+      "_id"
+    ),
   });
+  await todoOfDestinationUser.save();
+  await Todo.findByIdAndDelete(sourceUserId);
+
+  return {
+    success: true,
+  };
 };
 
-const updateTodo = async (req, res, next, sendResponse = true) => {
-  catchError(next, async () => {
-    const { userId } = req;
-    if (req.body.data?.todos) data = req.body.data.todos;
-    else data = req.body.data.todo;
-    const itemListData = _.isArray(data)
-      ? data.map((item) => renameObjectKey(item, "id", "_id"))
-      : [renameObjectKey(data, "id", "_id")];
+const updateTodo = async (req) => {
+  const { userId } = req;
+  if (req.body.data?.todos) data = req.body.data.todos;
+  else data = req.body.data.todo;
+  const itemListData = _.isArray(data)
+    ? data.map((item) => renameObjectKey(item, "id", "_id"))
+    : [renameObjectKey(data, "id", "_id")];
 
-    if (req.body.data?.todoSettings) {
-      await updateCustomization(req, res, next, false);
-    }
+  if (req.body.data?.todoSettings) {
+    await updateCustomization(req);
+  }
 
-    let todo = await Todo.findById(userId);
+  let todo = await Todo.findById(userId);
 
-    if (!todo) {
-      const newUserTodo = new Todo({
-        _id: userId,
-        itemList: [...itemListData],
-      });
-      await newUserTodo.save();
-
-      if (sendResponse) {
-        return res.status(201).json({ success: true });
-      } else {
-        return newUserTodo;
-      }
-    }
-
-    todo = _.extend(todo, {
-      itemList: addOrMergeArrayElements(todo.itemList, itemListData, "_id"),
+  if (!todo) {
+    const newUserTodo = new Todo({
+      _id: userId,
+      itemList: [...itemListData],
     });
-    await todo.save();
+    await newUserTodo.save();
 
-    if (sendResponse) {
-      return res.json({ success: true });
-    } else {
-      return todo;
-    }
+    return { success: true };
+  }
+
+  todo = _.extend(todo, {
+    itemList: addOrMergeArrayElements(todo.itemList, itemListData, "_id"),
   });
+  await todo.save();
+
+  return { success: true };
 };
 
 module.exports = {
